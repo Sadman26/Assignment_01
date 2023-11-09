0dc443d216133d825a6ab5a27b06eb6b0387555c
Sadman26 <fareless.ranger@gmail.com>
2023-11-09 22:37:48 +0600

    added logo and some js

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-function toggleMobileMenu() {
-    var mobileMenu = document.getElementById("mobileMenu");
+function toggleDarkMode() {
     let nvbr = document.getElementById("navbar");
-    let mblitm = document.getElementsByClassName("navbar__item");
-    let logo = document.querySelector(".navbar__logo");
+    let nvbritm = document.querySelector(".navbar__logo");
+    let links = document.querySelectorAll(".navbar__list a");
+    let mobileMenu = document.getElementById("mobileMenu");
+    let mobileLinks = document.querySelectorAll(".mobile-menu a");
 
-    if (mobileMenu.style.display === "block") {
-        mobileMenu.style.display = "none";
-        nvbr.style.boxShadow = "none"; // Remove box shadow
-        mblitm.style.padding = "0"; // Reset padding
-        logo.style.textAlign = "left"; // Align the logo to the left
+    if (nvbr.style.backgroundColor === "white") {
+        // Desktop mode dark theme
+        nvbr.style.backgroundColor = "black";
+        nvbritm.style.color = "white";
+        for (let i = 0; i < links.length; i++) {
+            links[i].style.color = "white";
+        }
+
+        // Mobile mode dark theme
+        mobileMenu.style.backgroundColor = "black";
+        for (let i = 0; i < mobileLinks.length; i++) {
+            mobileLinks[i].style.color = "white";
+        }
     } else {
-        mobileMenu.style.display = "block";
-        nvbr.style.boxShadow = "0 0 0 1000px rgba(0, 0, 0, 0.5)";
-        mblitm.style.padding = "15px 10px";
-        logo.style.textAlign = "center"; // Center align the logo
+        // Desktop mode light theme
+        nvbr.style.backgroundColor = "white";
+        nvbritm.style.color = "black";
+        for (let i = 0; i < links.length; i++) {
+            links[i].style.color = "black";
+        }
+
+        // Mobile mode light theme
+        mobileMenu.style.backgroundColor = "white";
+        for (let i = 0; i < mobileLinks.length; i++) {
+            mobileLinks[i].style.color = "black";
+        }
     }
 }
+
+let sun = document.getElementById("sunicon");
+sun.addEventListener("click", toggleDarkMode);
