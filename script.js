9fc65d4aadeaa268b38b374fcbb612cbb0f093b2
Sadman26 <fareless.ranger@gmail.com>
2023-11-09 22:56:00 +0600

    some change

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ function toggleDarkMode() {
     let links = document.querySelectorAll(".navbar__list a");
     let mobileMenu = document.getElementById("mobileMenu");
     let mobileLinks = document.querySelectorAll(".mobile-menu a");
+    let bdy = document.querySelector("body");
     if (nvbr.style.backgroundColor === "white") {
         nvbr.style.backgroundColor = "black";
         nvbritm.style.color = "white";
+        bdy.style.backgroundColor = "white";
         for (let i = 0; i < links.length; i++) {
             links[i].style.color = "white";
         }
@@ -14,8 +16,11 @@ function toggleDarkMode() {
         for (let i = 0; i < mobileLinks.length; i++) {
             mobileLinks[i].style.color = "white";
         }
+        nvbritm.style.textAlign = "center";
     } else {
+        // Desktop mode light theme
         nvbr.style.backgroundColor = "white";
+        bdy.style.backgroundColor = "white";
         nvbritm.style.color = "black";
         for (let i = 0; i < links.length; i++) {
             links[i].style.color = "black";
@@ -24,7 +29,9 @@ function toggleDarkMode() {
         for (let i = 0; i < mobileLinks.length; i++) {
             mobileLinks[i].style.color = "black";
         }
+        nvbritm.style.textAlign = "center";
     }
 }
+
 let sun = document.getElementById("sunicon");
 sun.addEventListener("click", toggleDarkMode);
